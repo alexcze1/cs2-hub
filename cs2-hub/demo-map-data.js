cd118c3b3692c24ec0a587c6a3a0c3b958bae2bb
alexcze1 <lofqvistalex@gmail.com>
2026-04-28 14:24:03 +0200

    fix: align de_overpass spacing in MAP_DATA

--- cs2-hub/demo-map-data.js
+++ cs2-hub/demo-map-data.js
@@ -8,7 +8,7 @@ export const MAP_DATA = {
   de_dust2:   { pos_x: -2476, pos_y:  3239, scale: 4.40 },
   de_vertigo: { pos_x: -3168, pos_y:  1762, scale: 4.00 },
   de_train:   { pos_x: -2477, pos_y:  2392, scale: 4.70 },
-  de_overpass: { pos_x: -4831, pos_y:  1781, scale: 5.20 },
+  de_overpass:  { pos_x: -4831, pos_y:  1781, scale: 5.20 },
 }
 
 /**
